bfd065712eeac9f3d6311b2af1beab7153558839
szkertesz <sz.kertesz@mito.hu>
2019-11-07 01:07:12 +0100

    Try to implement appending sheets

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function scrape() {
         await page.goto('https://start.mokk.hu/kozjegyzokereso.html')
         await page.waitForSelector('input.gwt-SuggestBox');
 
-        for (let i = 0; i < 2; i++) {
+        for (let i = 0; i < 3; i++) {
             await page.type('input.gwt-SuggestBox', `Budapest  ${i + 1}. kerület`)
             await page.click('button.gwt-Button');
             await page.waitFor(500);
@@ -62,66 +62,75 @@ async function scrape() {
     }
 }
 
-(function createSheets() {
-// (async function createSheets() {
-    // const returnedData = await scrape();
+// (function createSheets() {
+(async function createSheets() {
+    const returnedData = await scrape();
     // console.log(returnedData);
     // const sheetName = 'sheeet';
     // const sheet = XLSX.utils.json_to_sheet(returnedData[1]);
     // const wb = XLSX.utils.book_new();
     // XLSX.writeFile(wb, sheet);
-    const data = [[{
-        name: 'dr. Józsa Krisztina Marianna',
-        address: '1015 Budapest,Hattyú utca 16. IV/8.',
-        phone: '+36 (30) 479 4796, +36 (1) 225 8390, +36 (1) 225 8391',
-        email: 'jozsa@mokk.hu',
-        web: 'https:\\\\jozsa.kozjegyzok.mokk.hu'
-    },
-    {
-        name: 'dr. Andrásiné dr. Szabó Emőke',
-        address: '1015 Budapest,Batthyány utca 10. I/1.',
-        phone: '+36 (1) 225 0286, +36 (1) 225 0287',
-        email: 'szaboemo@mokk.hu',
-        web: 'https:\\\\szaboemo.kozjegyzok.mokk.hu'
-    }],
-    [{
-        name: 'dr. Asbóth-Hermányi Lőrinc Bence',
-        address: '1024 Budapest,Szilágyi Erzsébet fasor 1. fszt. 1.',
-        phone: '+36 (1) 786 9487, +36 (1) 786 9578',
-        email: 'asboth-hermanyi.lorinc@mokk.hu',
-        web: 'https:\\\\asboth-hermanyilorinc.kozjegyzok.mokk.hu'
-    },
-    {
-        name: 'dr. Berberovics Mirjana',
-        address: '1022 Budapest,Füge utca 2/B. FSZT. 1-2.',
-        phone: '+36 (1) 613 0140',
-        email: 'berberovics@mokk.hu',
-        web: 'https:\\\\berberovics.kozjegyzok.mokk.hu'
-    },
-    {
-        name: 'dr. Varga Krisztina',
-        address: '1027 Budapest,Csalogány utca 21. I/3.',
-        phone: '+36 (1) 202 4276, +36 (1) 213 9433',
-        email: 'vargakrisztina@mokk.hu, iroda.vk@kozjegyzo.hu',
-        web: 'https:\\\\vargakrisztina.kozjegyzok.mokk.hu'
-    },
-    {
-        name: 'dr. Szalontai Magdolna Rozália',
-        address: '1027 Budapest,Csalogány utca 7. I/1.',
-        phone: '+36 (1) 225 3307, +36 (1) 225 3308',
-        email: 'szalontai@mokk.hu',
-        web: 'https:\\\\szalontai.kozjegyzok.mokk.hu'
-    }]];
+    // const data = [[{
+    //     name: 'dr. Józsa Krisztina Marianna',
+    //     address: '1015 Budapest,Hattyú utca 16. IV/8.',
+    //     phone: '+36 (30) 479 4796, +36 (1) 225 8390, +36 (1) 225 8391',
+    //     email: 'jozsa@mokk.hu',
+    //     web: 'https:\\\\jozsa.kozjegyzok.mokk.hu'
+    // },
+    // {
+    //     name: 'dr. Andrásiné dr. Szabó Emőke',
+    //     address: '1015 Budapest,Batthyány utca 10. I/1.',
+    //     phone: '+36 (1) 225 0286, +36 (1) 225 0287',
+    //     email: 'szaboemo@mokk.hu',
+    //     web: 'https:\\\\szaboemo.kozjegyzok.mokk.hu'
+    // }],
+    // [{
+    //     name: 'dr. Asbóth-Hermányi Lőrinc Bence',
+    //     address: '1024 Budapest,Szilágyi Erzsébet fasor 1. fszt. 1.',
+    //     phone: '+36 (1) 786 9487, +36 (1) 786 9578',
+    //     email: 'asboth-hermanyi.lorinc@mokk.hu',
+    //     web: 'https:\\\\asboth-hermanyilorinc.kozjegyzok.mokk.hu'
+    // },
+    // {
+    //     name: 'dr. Berberovics Mirjana',
+    //     address: '1022 Budapest,Füge utca 2/B. FSZT. 1-2.',
+    //     phone: '+36 (1) 613 0140',
+    //     email: 'berberovics@mokk.hu',
+    //     web: 'https:\\\\berberovics.kozjegyzok.mokk.hu'
+    // },
+    // {
+    //     name: 'dr. Varga Krisztina',
+    //     address: '1027 Budapest,Csalogány utca 21. I/3.',
+    //     phone: '+36 (1) 202 4276, +36 (1) 213 9433',
+    //     email: 'vargakrisztina@mokk.hu, iroda.vk@kozjegyzo.hu',
+    //     web: 'https:\\\\vargakrisztina.kozjegyzok.mokk.hu'
+    // },
+    // {
+    //     name: 'dr. Szalontai Magdolna Rozália',
+    //     address: '1027 Budapest,Csalogány utca 7. I/1.',
+    //     phone: '+36 (1) 225 3307, +36 (1) 225 3308',
+    //     email: 'szalontai@mokk.hu',
+    //     web: 'https:\\\\szalontai.kozjegyzok.mokk.hu'
+    // }]];
 
     const fileName = 'sheeet.xlsx';
-    const sheetName = 'Sheeet';
-    const sheet = XLSX.utils.json_to_sheet(data[1]);
+    const dataLength = returnedData.length;
+    
     const wb = XLSX.utils.book_new();
+    // works with single sheet
+    // for (let i = 0; i < dataLength; i++) {
+    //     let sheetName = `Sheeet${i}`;
+    //     let sheet = XLSX.utils.json_to_sheet(returnedData[i]);
+    //     XLSX.utils.book_append_sheet(wb, sheet, sheetName);
+    // }
+    const sheetName = `Sheeet`;
+    const sheet = XLSX.utils.json_to_sheet(returnedData[0]);
+    for (let i = 1; i < dataLength; i++) {
+        XLSX.utils.sheet_add_json(sheet, returnedData[i], {skipHeader: true, origin: `A${returnedData[i - 1].length}`});
+        console.log(i);
+    }
     XLSX.utils.book_append_sheet(wb, sheet, sheetName);
     
     // const result = XLSX.write(wb, {});
     XLSX.writeFile(wb, fileName);
-
-// // Write to a file
-// writeFile('./hello.xlsx',result)
 })();
